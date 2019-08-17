e96f523cc545f4be2f0469a1cd775399c4a559b9
Torry Tufteland <torry.tufteland@gmail.com>
2019-08-17 14:29:14 +0200

    Add useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const requestTimeBeforePending = 600;
+
+function useFetchData<T>(func: () => Promise<T>) {
+  const [pending, setPending] = useState(false);
+  const [delayedPending, setDelayedPending] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([] as T[]);
+  useEffect(() => {
+    async function fetchData() {
+      setPending(true);
+      setSuccess(false);
+      setError(false);
+      const timeoutRef = setTimeout(() => {
+        setDelayedPending(true);
+      }, requestTimeBeforePending);
+
+      try {
+        await func();
+        clearTimeout(timeoutRef);
+        setSuccess(true);
+        setPending(false);
+        setData(data as T[]);
+      } catch (err) {
+        clearTimeout(timeoutRef);
+        setError(true);
+        setPending(false);
+      }
+    }
+    fetchData();
+  });
+  return [data, pending, delayedPending, error, success] as [
+    T[],
+    boolean,
+    boolean,
+    boolean,
+    boolean
+  ];
+}
+
+export { useFetchData };
